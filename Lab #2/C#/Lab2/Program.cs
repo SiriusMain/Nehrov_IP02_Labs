be869f9c1d56c818616a1475de8f6086db8c3153
Andrey Nehrov <andrey.negrov@gmail.com>
2020-10-27 21:52:25 +0200

    Initial commit

---
+++ Lab #2/C#/Lab2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+
+namespace Lab2
+{
+    class Program
+    {
+        public static double[] getSidesValues(double AX, double AY, double BX, double BY, double CX, double CY)
+        {
+            double[] buff = new double[3];
+
+            buff[0] = Math.Sqrt(Math.Pow(BX - AX, 2) + Math.Pow((BY - AY), 2));
+            buff[1] = Math.Sqrt(Math.Pow(CX - BX, 2) + Math.Pow((CY - BY), 2));
+            buff[2] = Math.Sqrt(Math.Pow(CX - AX, 2) + Math.Pow((CY - AY), 2));
+
+            return buff;
+        }
+
+        public static string getTriangleType(double[] Sides)
+        {
+            if (Sides[0] == Sides[1] && Sides[1] == Sides[2] && Sides[0] == Sides[2]) { return "Рівносторонній"; }
+            else if (Sides[0] == Sides[1] || Sides[1] == Sides[2] || Sides[0] == Sides[2]) { return "Рівнобедрений"; }
+            return "Різносторонній";
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Трикутник ABC: {getTriangleType(getSidesValues(0, 3, -2, 0, 0, 3))}");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Lab #2/JavaScript/main.js
@@ -0,0 +1,18 @@
+function getSidesValues(AX, AY, BX, BY, CX, CY) {
+    let buff = [];
+
+    buff[0] = Math.sqrt(Math.pow(BX - AX, 2) + Math.pow((BY - AY), 2));
+    buff[1] = Math.sqrt(Math.pow(CX - BX, 2) + Math.pow((CY - BY), 2));
+    buff[2] = Math.sqrt(Math.pow(CX - AX, 2) + Math.pow((CY - AY), 2));
+
+    return buff;
+};
+
+
+function getTriangleType (Sides){ 
+    if (Sides[0] == Sides[1] && Sides[1] == Sides[2] && Sides[0] == Sides[2]) { return "Рівносторонній"; }
+    else if (Sides[0] == Sides[1] || Sides[1] == Sides[2] || Sides[0] == Sides[2]) { return "Рівнобедрений"; }
+    return "Різносторонній";
+};
+
+console.log("Трикутник ABC: " + getTriangleType(getSidesValues(0, 3, -2, 0, 0, 3)));
